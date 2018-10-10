e7fe74de88d2805471e824d7b3c8114f14ba8e8c
MattiasLass <mattias.lass@gmail.com>
2018-10-10 18:54:56 +0300

    adds hostModel functionality

--- src/main/java/com/github/ltat_06_007_project/Models/HostModel.java
+++ src/main/java/com/github/ltat_06_007_project/Models/HostModel.java
@@ -3,10 +3,24 @@ package com.github.ltat_06_007_project.Models;
 import com.github.ltat_06_007_project.Objects.HostObject;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Set;
+
 @Component
 public class HostModel {
 
-    public void updateHost(HostObject host) {
+    private final HashMap<String,String> hostIpToHostName = new HashMap<String, String>();
 
+    public void updateHost(HostObject host) {
+        hostIpToHostName.put(host.getAddress(),host.getName());
     }
+
+    public Set<String> getAllIps() {
+        return hostIpToHostName.keySet();
+    }
+
+    public String getHostName(String ip) {
+        return hostIpToHostName.get(ip);
+    }
+    
 }
--- src/main/java/com/github/ltat_06_007_project/Objects/HostObject.java
+++ src/main/java/com/github/ltat_06_007_project/Objects/HostObject.java
@@ -2,6 +2,11 @@ package com.github.ltat_06_007_project.Objects;
 
 public class HostObject {
     private String name;
+
+    public String getAddress() {
+        return address;
+    }
+
     private String address;
 
     public HostObject(String name, String address) {
