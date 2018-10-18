a1e4ad61377efd1d0ea4f4bfc3b5c032a8e1f1d5
Andrew Lei <andrew.w.lei@gmail.com>
2018-10-18 15:06:47 +0300

    Use symmetric encryption for message

--- src/main/java/com/mycompany/Crypto.java
+++ src/main/java/com/mycompany/Crypto.java
@@ -5,6 +5,7 @@ import java.security.spec.*;
 import java.io.*;
 import java.nio.file.*;
 import javax.crypto.*;
+import javax.crypto.spec.*;
 
 public class Crypto {
     public static void genKeyPair(String outFile) throws NoSuchAlgorithmException, IOException {
@@ -13,14 +14,6 @@ public class Crypto {
         KeyPair kp = keyGen.generateKeyPair();
         byte[] publicKey = kp.getPublic().getEncoded();
         byte[] privateKey = kp.getPrivate().getEncoded();
-        StringBuffer retString = new StringBuffer();
-        for (int i = 0; i < publicKey.length; ++i) {
-            retString.append(Integer.toHexString(0x0100 + (publicKey[i] & 0x00FF)).substring(1));
-        }
-        StringBuffer retString2 = new StringBuffer();
-        for (int i = 0; i < privateKey.length; ++i) {
-            retString2.append(Integer.toHexString(0x0100 + (privateKey[i] & 0x00FF)).substring(1));
-        }
 
         FileOutputStream out = new FileOutputStream(outFile + ".key");
         out.write(privateKey);
@@ -29,6 +22,12 @@ public class Crypto {
         out = new FileOutputStream(outFile + ".pub");
         out.write(publicKey);
         out.close();
+    }
+
+    public static SecretKey genAESKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(256);
+        return keyGen.generateKey();
     }
 
     public static PublicKey readPub(String filepath) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
@@ -42,6 +41,10 @@ public class Crypto {
         return kf.generatePublic(ks);
     }
 
+    public static byte[] readPubBytes(String filepath) throws IOException {
+        return Files.readAllBytes(Paths.get(filepath));
+    }
+
     public static PrivateKey readKey(String filepath) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
         /* Read all bytes from the private key file */
         Path path = Paths.get(filepath);
@@ -52,22 +55,29 @@ public class Crypto {
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return kf.generatePrivate(ks);
     }
-    public static byte[] encrypt(PublicKey pub, String text) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException {
+
+    public static byte[] encryptSymKey(PublicKey pub, SecretKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, pub);
-        return cipher.doFinal(text.getBytes("UTF8"));
+        return cipher.doFinal(key.getEncoded());
     }
 
-
-    public static String decrypt(PrivateKey key, byte[] enctext) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException {
+    public static SecretKey decryptSymKey(PrivateKey key, byte[] encKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, key);
-        String decText = new String(cipher.doFinal(enctext));
-        return decText;
+        byte[] decKey = cipher.doFinal(encKey);
+        return new SecretKeySpec(decKey, 0, decKey.length, "AES");
     }
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
-        genKeyPair("foobar");
-        PrivateKey pvt = readKey("foobar.key");
+    public static byte[] encryptText(SecretKey key, byte[] text) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(text);
+    }
+
+    public static byte[] decryptText(SecretKey key, byte[] encText) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(encText);
     }
 }
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,9 +11,13 @@ import org.digidoc4j.SignatureBuilder;
 import org.digidoc4j.signers.PKCS12SignatureToken;
 
 import java.io.IOException;
+import java.io.FileOutputStream;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.PrivateKey;
+import javax.crypto.SecretKey;
 
 import com.mycompany.crypto.Crypto;
 
@@ -21,39 +25,61 @@ public class App
 {
     public static void main( String[] args ) throws Exception //NoSuchAlgorithmException, IOException 
     {
-        Crypto.genKeyPair("data/test");
+        if (args.length > 0) {
+            if (args[0].equals("genkey")) {
+                Crypto.genKeyPair("data/test");
 
-        Configuration config = new Configuration(Mode.TEST);
-        //Create a container with two text files to be signed
-        Container container = ContainerBuilder.
-            aContainer().
-            withConfiguration(config).
-            withDataFile("data/test.pub", "text/plain").
-            build();
+            }
+            else if (args[0].equals("signkey")) {
+                Configuration config = new Configuration(Mode.TEST);
+                //Create a container with two text files to be signed
+                Container container = ContainerBuilder.
+                    aContainer().
+                    withConfiguration(config).
+                    withDataFile("data/test.pub", "text/plain").
+                    build();
 
-        //Using the private key stored in the "signout.p12" file with password "test"
-        String privateKeyPath = "data/certificate.p12";
-        char[] password = "test".toCharArray();
-        PKCS12SignatureToken signatureToken = new PKCS12SignatureToken(privateKeyPath, password);
-        System.out.println(signatureToken);
-        //Create a signature
-        Signature signature = SignatureBuilder.
-            aSignature(container).
-            withSignatureToken(signatureToken).
-            invokeSigning();
-        System.out.println(signature);
-        //Add the signature to the container
-        container.addSignature(signature);
+                //Using the private key stored in the "signout.p12" file with password "test"
+                String privateKeyPath = "data/certificate.p12";
+                char[] password = "test".toCharArray();
+                PKCS12SignatureToken signatureToken = new PKCS12SignatureToken(privateKeyPath, password);
+                System.out.println(signatureToken);
+                //Create a signature
+                Signature signature = SignatureBuilder.
+                    aSignature(container).
+                    withSignatureToken(signatureToken).
+                    invokeSigning();
+                System.out.println(signature);
+                //Add the signature to the container
+                container.addSignature(signature);
 
-        //Save the container as a .bdoc file
-        container.saveAsFile("data/test-container.bdoc");
+                //Save the container as a .bdoc file
+                container.saveAsFile("data/test-container.bdoc");
+            }
+            else if (args[0].equals("encrypt")) {
+                PublicKey pub = Crypto.readPub("data/test.pub");
+                SecretKey key = Crypto.genAESKey();
 
-        PrivateKey key = Crypto.readKey("data/test.key");
-        PublicKey pub = Crypto.readPub("data/test.pub");
+                byte[] encKey = Crypto.encryptSymKey(pub, key);
+                FileOutputStream out = new FileOutputStream("data/key.enc");
+                out.write(encKey);
+                out.close();
 
-        byte[] enctext = Crypto.encrypt(pub, "Hello world!");
-        System.out.println(new String(enctext));
-        System.out.println(Crypto.decrypt(key, enctext));
+                byte[] encText = Crypto.encryptText(key, "Hello world!".getBytes("UTF8"));
+                FileOutputStream out2 = new FileOutputStream("data/message.enc");
+                out2.write(encText);
+                out2.close();
+            }
+            else if (args[0].equals("decrypt")) {
+                PrivateKey pvt = Crypto.readKey("data/test.key");
+                byte[] encKey = Files.readAllBytes(Paths.get("data/key.enc"));
+                byte[] encText = Files.readAllBytes(Paths.get("data/message.enc"));
 
+                SecretKey key = Crypto.decryptSymKey(pvt, encKey);
+                byte[] decText = Crypto.decryptText(key, encText);
+                
+                System.out.println(new String(decText));
+            }
+        }
    }
 }
