596dde530e3413b7887400889dd57e8b714c0cfd
MattiasLass <mattias.lass@gmail.com>
2018-10-26 00:21:13 +0300

    creates tcp connection class

--- src/main/java/com/github/ltat_06_007_project/Controllers/ConnectionController.java
+++ src/main/java/com/github/ltat_06_007_project/Controllers/ConnectionController.java
@@ -5,27 +5,22 @@ import com.github.ltat_06_007_project.Objects.ConnectionObject;
 import com.github.ltat_06_007_project.Objects.ContactObject;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.IOException;
-import java.util.AbstractQueue;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ConnectionController {
 
-    private List<ConnectionObject> activeConnections = new ArrayList<>();
-    private List<ContactObject> pendingConnections = new CopyOnWriteArrayList<>();
+    private LinkedBlockingQueue<ContactObject> pendingConnections = new LinkedBlockingQueue<>();
     private ContactModel contactModel;
 
     @Autowired
     public ConnectionController(ContactModel contactModel) {
         this.contactModel = contactModel;
-
         Runnable connectionHandler = this::createConnections;
         new Thread(connectionHandler).start();
-
     }
 
     private void createConnections() {
@@ -34,29 +29,19 @@ public class ConnectionController {
                 .filter(c -> c.getAllowed())
                 .forEach(c -> pendingConnections.add(c));
 
+        Executor executor = Executors.newCachedThreadPool();
         while (!Thread.interrupted()) {
-
-            //for (ContactObject contact : pendingConnections.)
-
             try {
-                Thread.sleep(5000);
+                executor.execute(new TcpConnetcion(pendingConnections.take(), this)::establishConnection);
             } catch (InterruptedException e) {
                 break;
             }
         }
-
     }
-    /*
-    private void establishConnection(ContactObject contact) {
-        var connection = new ConnectionObject();
-        try {
-            //TODO create a persistent tcp connection with the target on the last known ip
-            throw new IOException();
-            activeConnections.add(connection);
-            //TODO log connection created
-        } catch (IOException e) {
-            //TODO log failed connection
-            pendingConnections.add(contact);
-        }
-    }*/
+
+    public void put(ContactObject contact) throws InterruptedException {
+        pendingConnections.put(contact);
+    }
+
+
 }
---
+++ src/main/java/com/github/ltat_06_007_project/Controllers/TcpConnetcion.java
@@ -0,0 +1,34 @@
+package com.github.ltat_06_007_project.Controllers;
+
+import com.github.ltat_06_007_project.Objects.ContactObject;
+
+import java.io.IOException;
+
+public class TcpConnetcion {
+
+    private final ContactObject contact;
+    private final ConnectionController connectionController;
+
+
+    public TcpConnetcion(ContactObject contact, ConnectionController connectionController) {
+        this.contact = contact;
+        this.connectionController = connectionController;
+    }
+
+    public void establishConnection() {
+        try {
+            //var connection = new ConnectionObject();
+            //TODO create a persistent tcp connection with the target on the last known ip
+            throw new IOException();
+            //activeConnections.add(connection);
+            //TODO log connection created
+        } catch (IOException e) {
+            //TODO log failed connection
+            try {
+                Thread.sleep(5000);
+                connectionController.put(contact);
+            } catch (InterruptedException ex) {
+            }
+        }
+    }
+}
