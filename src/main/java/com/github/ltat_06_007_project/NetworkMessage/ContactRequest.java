b1a816cc8c6d1e2641111d4f6302b864b99d300c
MattiasLass <mattias.lass@gmail.com>
2018-10-29 20:23:23 +0200

    makes contact request serializable

--- src/main/java/com/github/ltat_06_007_project/NetworkMessage/ContactRequest.java
+++ src/main/java/com/github/ltat_06_007_project/NetworkMessage/ContactRequest.java
@@ -1,11 +1,15 @@
 package com.github.ltat_06_007_project.NetworkMessage;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ContactRequest {
 
     private final String requesterIp;
     private final String reqiesteeIp;
 
-    public ContactRequest(String requesterIp, String reqiesteeIp) {
+    @JsonCreator
+    public ContactRequest(@JsonProperty("requesterIp")String requesterIp, @JsonProperty("reqiesteeIp")String reqiesteeIp) {
         this.requesterIp = requesterIp;
         this.reqiesteeIp = reqiesteeIp;
     }
