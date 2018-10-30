2f719c08600c3277fcf72df04f3c12af2be3ab1c
MattiasLass <mattias.lass@gmail.com>
2018-10-30 12:38:43 +0200

    makes messageobject serializable

--- src/main/java/com/github/ltat_06_007_project/Objects/MessageObject.java
+++ src/main/java/com/github/ltat_06_007_project/Objects/MessageObject.java
@@ -1,10 +1,14 @@
 package com.github.ltat_06_007_project.Objects;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class MessageObject {
     private final String content;
     private final String contactId;
 
-    public MessageObject(String content) {
+    @JsonCreator
+    public MessageObject(@JsonProperty("content")String content) {
         this.content = content;
         contactId = "";
     }
